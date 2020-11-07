317463d7dea0d6f580f976e9b9b349842749c2a5
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-11-07 09:10:16 -0600

    Use useState to hold the data array and pass it through a List prop.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
+
 function App() {
+  // put the list (array) in a state variable (people)
+  const [people, setPeople] = useState(data);
+
   return (
     <main>
       <section className="container">
-        <h1 className="h3">0 birthdays today</h1>
-        <List />
+        {/* count the array */}
+        <h1 className="h3">{people.length} birthdays today</h1>
+        {/* pass the list of people through a people prop */}
+        <List people={people} />
         <button onClick={() => console.log('you clicked me!')}>
           clear all
         </button>
