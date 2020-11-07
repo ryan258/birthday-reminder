1a700463bdf55443de34d3ac616e4f5e7178c8b5
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-11-07 09:23:37 -0600

    Map out the people array in the List component.

--- src/List.js
+++ src/List.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
-const List = () => {
+// deconstruct the people data from the props being passed in from App
+const List = ({ people }) => {
   return (
     <>
-      <h2>list component</h2>
+      {/* map over the array */}
+      {people.map((person) => {
+        // decontruct properties from each object in the array
+        const { id, name, age, image } = person;
+        // return a new array item with html and the person's data
+        return (
+          <article key={id} className="person">
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years old</p>
+            </div>
+          </article>
+        );
+      })}
     </>
   );
 };
