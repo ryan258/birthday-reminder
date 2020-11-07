dba249d34b824d7842c3c202382dc6ca3b9550e9
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-11-07 09:00:02 -0600

    Set up html and adjust styles.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  return (
+    <main>
+      <section className="container">
+        <h1 className="h3">0 birthdays today</h1>
+        <List />
+        <button onClick={() => console.log('you clicked me!')}>
+          clear all
+        </button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
